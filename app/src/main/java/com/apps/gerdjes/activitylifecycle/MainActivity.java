319102276e77a5c5ee44da2cf531ac6eb960e2dc
Gerdje Hung-chu <gerdje.hc@gmail.com>
2017-10-09 14:57:53 +0200

    Initial commit- Final

---
+++ app/src/main/java/com/apps/gerdjes/activitylifecycle/MainActivity.java
@@ -0,0 +1,50 @@
+package com.apps.gerdjes.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private String TAG = "AndroidATC Lesson 2";
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart invoked");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume invoked");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause invoked");
+
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop invoked");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy invoked");
+    }
+
+}
